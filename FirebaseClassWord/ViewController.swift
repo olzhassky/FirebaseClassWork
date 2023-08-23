ac0008aa0957b84d988aabc2daf5ebd1115e18d8
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-08-23 19:57:16 +0600

    basic

--- FirebaseClassWord/ViewController.swift
+++ FirebaseClassWord/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SnapKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    let items: [String] = ["hello", "world"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        view.backgroundColor = .white
+        setupView()
+        makeConstriants()
     }
 
 
 }
 
+private extension ViewController {
+    func setupView() {
+        title = "Firebase"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+      
+
+    }
+    func makeConstriants() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    
+}
